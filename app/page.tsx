c575eb17513a53d35d11ea08f9de002f8e95696f
Mustafa Khan <mustafakhanneo@gmail.com>
2024-05-26 20:23:27 +0500

    testiei

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,32 @@ import Banner from './components/Banner';
 
 const Home = () => {
 
+  const banner = [{
+    url: "https://picsum.photos/1530/500",
+    title: "First",
+    description: "This is first banner."
+  },
+  {
+    url: "https://picsum.photos/1530/500",
+    title: "Second",
+    description: "This is Second banner."
+  },
+  {
+    url: "https://picsum.photos/1530/500",
+    title: "Third",
+    description: "This is Third banner."
+  },
+  {
+    url: "https://picsum.photos/1530/500",
+    title: "Fourth",
+    description: "This is Fourth banner."
+  },
+  ]
+
   return (
     <div  className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]">
 <Navbar />
-<Banner />
+<Banner banner={banner} />
 <Footer />
       
     </div>
